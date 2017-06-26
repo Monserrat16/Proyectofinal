8f6192f3a0e378341ffb649f6dd0288f56abc397
Yolanda M. Garcia <yolanda.garcia16@tectijuana.edu.mx>
2017-06-26 12:00:23 -0700

    Creando nuevo proyecto del blog

---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class ListCreateCategoriesTest < ActionDispatch::IntegrationTest
+   
+   def setup
+      @category = Category.create(name: "sports")
+      @category2 = Category.create(name: "Programming")
+  end
+
+
+  test "should show categories listing" do
+    get categories_path
+    assert_template 'categories/index'
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category), text: @category2.name
+  end
+
+end
